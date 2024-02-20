39823511b1bc7935093bd35dae0d125406386b62
Eshwar Reddy <eshwarreddy.thummala@gmail.com>
2024-02-20 11:53:04 -0700

    Added new filters id_filter, username_filter

--- utils/filter_utils.py
+++ utils/filter_utils.py
@@ -1,6 +1,8 @@
 from utils.filters import filter_based_on_avg_views
 from utils.filters import filter_based_on_followers_count
 from utils.filters import filter_based_on_keyword
+from utils.filters import filter_based_on_id_available
+from utils.filters import filter_based_on_username
 from utils.data_import import load_json_data
 from multiprocessing import Pool
 import os
@@ -9,21 +11,35 @@ def get_filtered_data(filters, app):
    filter_function_mapper = {
       'follower_filter':filter_based_on_followers_count,
       'keyword_filter': filter_based_on_keyword,
-      'avgview_filter': filter_based_on_avg_views
+      'avgview_filter': filter_based_on_avg_views,
+      'socialid_filter': filter_based_on_id_available,
+      'username_filter' : filter_based_on_username
+
       }
-   filtered_data = load_json_data(os.path.join(app.root_path,'static','user-info.json'))
-   for filter_key, filter_criteria in filters.items():
-      if(len(filtered_data) != 0):
-         filtered_data = parallel_filter(filtered_data, filter_criteria, filter_function_mapper[filter_key])
-   return filtered_data
+   user_data = load_json_data(os.path.join(app.root_path,'static','user-info.json'))
+   return parallel_filter(user_data, filters, filter_function_mapper)
 
 
-def parallel_filter(data, filter_criteria, filter_function):
+def parallel_filter(data, filters, filter_function_mapper):
   if(len(data) <= 100):
-     return filter_function(data, filter_criteria)
+     return apply_filters(data, filters, filter_function_mapper)
   number_of_process = 3
   chunk_size = len(data)//number_of_process
   data_chunks = [data[i:None if i==(3*chunk_size) else i+chunk_size] for i in range(0,len(data),chunk_size)]
   with Pool(processes=number_of_process) as pool:
-    results = pool.starmap(filter_function, [(chunk, filter_criteria) for chunk in data_chunks])
+    results = pool.starmap(apply_filters, [(chunk, filters, filter_function_mapper) for chunk in data_chunks])
   return [json_obj for json_list in results for json_obj in json_list]
+
+def apply_filters(json_data, filters, filter_function_mapper):
+   current_filters = filters.keys()
+   filtered_data = []
+   for json_object in json_data:
+      passed_filter = True
+      for filter in current_filters:
+         if(not filter_function_mapper[filter](json_object, filters[filter])):
+            passed_filter = False
+      if(passed_filter):
+         filtered_data.append(json_object)
+   return filtered_data
+
+
--- utils/filters.py
+++ utils/filters.py
@@ -1,36 +1,43 @@
-import json
+import re
 
-def filter_based_on_followers_count(json_data, given_followers_count):
+def filter_based_on_followers_count(json_object, given_followers_count):
   minimum_followers, maximum_followers = given_followers_count
-  filter = 'tiktok_followers'
-  filtered_data = []
-  for json_object in json_data:
-    follower_count = json_object['user_profile'].get(filter)
-    if (follower_count != None and int(minimum_followers) <= follower_count <= int(maximum_followers)):
-      filtered_data.append(json_object)
-  return filtered_data
+  follower_count = json_object['user_profile'].get('tiktok_followers')
+  if (follower_count != None and int(minimum_followers) <= follower_count <= int(maximum_followers)):
+      return True
+  return False
 
-def filter_based_on_keyword(json_data, keyword):
+def filter_based_on_keyword(json_object, keyword):
   keyword = keyword.lower()
-  filter='tiktok_bio'
-  filtered_data = []
-  for json_object in json_data:
-    bio = (json_object['user_profile'].get(filter)).lower()
-    if (bio != None and keyword in bio):
-      filtered_data.append(json_object)
-  return filtered_data
+  bio = (json_object['user_profile'].get('tiktok_bio')).lower()
+  if (bio != None and keyword in bio):
+    return True
+  return False
 
-def filter_based_on_avg_views(json_data, given_avg_views):
-  filtered_data = []
-  for json_obj in json_data:
-    avg_views = 0
-    number_of_posts = 0
-    for post in json_obj['posts_info']:
-      if(post.get('views')):
-        avg_views += post['views']
-        number_of_posts += 1
-    if(number_of_posts != 0):
-      avg_views /= number_of_posts
-    if(avg_views >= int(given_avg_views)):
-      filtered_data.append(json_obj)
-  return filtered_data
+def filter_based_on_avg_views(json_object, given_avg_views):
+  avg_views = 0
+  number_of_posts = 0
+  for post in json_object.get('posts_info',[]):
+    if(post.get('views')):
+      avg_views += post['views']
+      number_of_posts += 1
+  if(number_of_posts != 0):
+    avg_views /= number_of_posts
+  if(avg_views >= int(given_avg_views)):
+    return True
+  return False
+
+def filter_based_on_id_available(json_object, given_social_id):
+  social_media_ids = json_object.get('social_media')
+  if(social_media_ids and social_media_ids.get(given_social_id)):
+    return True if social_media_ids.get(given_social_id) != '' else False
+  return False
+
+def filter_based_on_username(json_object, given_approximate_username):
+  user_profile = json_object.get('user_profile')
+  pattern = re.compile('.*('+given_approximate_username+').*', re.IGNORECASE)
+  if(user_profile and user_profile['username']):
+    search_result = pattern.search(user_profile['username'])
+    if(search_result):
+      return True
+  return False
